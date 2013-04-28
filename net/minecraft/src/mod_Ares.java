8e9ed5c4ffb42b2791eed1b89eec9617b6b5527d
palechip <palechip@gmail.com>
2013-04-28 21:30:07 +0200

    enable the map display (doesn't work with blitz)
    but I think they will add the servers to the play page soon

--- net/minecraft/src/mod_Ares.java
+++ net/minecraft/src/mod_Ares.java
@@ -143,26 +143,26 @@ public class mod_Ares extends BaseMod {
                 height += 8;
             }
             // Map fetcher:
-//            if (CONFIG.showMap && !AresData.isLobby) {
-//                if (AresData.getMap() != null) {
-//                    mc.fontRenderer.drawStringWithShadow("Current Map: \u00A7d" + AresData.getMap(), width, height, 16777215);
-//                    height += 8;
-//                } else {
-//                    AresData.setMap("Fetching...");
-//                    mc.fontRenderer.drawStringWithShadow("Current Map: \u00A78" + AresData.getMap(), width, height, 16777215);
-//                    height += 8;
-//                }
-//            }
-//            // Show next map
-//            if (CONFIG.showNextMap && !AresData.isLobby) {
-//                if (AresData.getNextMap() != null) {
-//                    mc.fontRenderer.drawStringWithShadow("Next Map: \u00A7d" + AresData.getNextMap(), width, height, 16777215);
-//                    height += 8;
-//                } else {
-//                    mc.fontRenderer.drawStringWithShadow("Next Map: \u00A78Loading...", width, height, 16777215);
-//                    height += 8;
-//                }
-//            }
+            if (CONFIG.showMap && !AresData.isLobby) {
+                if (AresData.getMap() != null) {
+                    mc.fontRenderer.drawStringWithShadow("Current Map: \u00A7d" + AresData.getMap(), width, height, 16777215);
+                    height += 8;
+                } else {
+                    AresData.setMap("Fetching...");
+                    mc.fontRenderer.drawStringWithShadow("Current Map: \u00A78" + AresData.getMap(), width, height, 16777215);
+                    height += 8;
+                }
+            }
+            // Show next map
+            if (CONFIG.showNextMap && !AresData.isLobby) {
+                if (AresData.getNextMap() != null) {
+                    mc.fontRenderer.drawStringWithShadow("Next Map: \u00A7d" + AresData.getNextMap(), width, height, 16777215);
+                    height += 8;
+                } else {
+                    mc.fontRenderer.drawStringWithShadow("Next Map: \u00A78Loading...", width, height, 16777215);
+                    height += 8;
+                }
+            }
             //Show KD Ratio
             if (CONFIG.showKD && !AresData.isLobby) {
                 mc.fontRenderer.drawStringWithShadow("K/D: \u00A73" + AresCustomMethods.getKD(), width, height, 16777215);
