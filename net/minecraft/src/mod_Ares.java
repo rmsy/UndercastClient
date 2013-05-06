bcd8aaf06b1ccd101255f1ae22e2117d82a3f99f
palechip <palechip@gmail.com>
2013-05-06 17:11:53 +0200

    Version Bump 1.5.2

--- net/minecraft/src/mod_Ares.java
+++ net/minecraft/src/mod_Ares.java
@@ -19,7 +19,7 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class mod_Ares extends BaseMod {
-    public final static String MOD_VERSION = "1.5.1";
+    public final static String MOD_VERSION = "1.5.2";
     protected String username = "Not_Found";
     protected Minecraft mc = Minecraft.getMinecraft();
     private boolean mainMenuActive;
