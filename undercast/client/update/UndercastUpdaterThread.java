93dded7b6af7d39d5ae2363c106080967e569848
palechip <palechip@gmail.com>
2013-05-08 19:04:15 +0200

    change the update link back

--- undercast/client/update/UndercastUpdaterThread.java
+++ undercast/client/update/UndercastUpdaterThread.java
@@ -33,7 +33,7 @@ public class UndercastUpdaterThread extends Thread{
         errorOccured = false;
         try {
             //download link
-            URL data = new URL("https://raw.github.com/palechip/UndercastMaintenance/master/version.txt"); // forge version should use forge instead of master
+            URL data = new URL("https://raw.github.com/UndercastTeam/UndercastClient/master/version.txt"); // forge version should use forge instead of master
             final BufferedReader in = new BufferedReader(new InputStreamReader(data.openStream()));
             readline = in.readLine();
             readline2 = in.readLine();
